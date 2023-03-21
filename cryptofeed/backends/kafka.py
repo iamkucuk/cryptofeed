f143bdb2907b1e0408d21e6b1e30e55850767d2f
Furkan Küçük <furkankucuk@outlook.com>
2023-03-21 21:14:57 +0300

    Update kafka.py

--- cryptofeed/backends/kafka.py
+++ cryptofeed/backends/kafka.py
@@ -15,10 +15,54 @@ from yapic import json
 
 from cryptofeed.backends.backend import BackendBookCallback, BackendCallback, BackendQueue
 
+import typing
+import confluent_kafka
+from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from uuid import uuid4
+from threading import Thread
+from confluent_kafka import KafkaException
+
 LOG = logging.getLogger('feedhandler')
 
+def to_dict_wrapper(obj:AvroModel, ctx):
+    return obj.asdict()
+
+class AIOProducer:
+    def __init__(self, configs, loop=None):
+        self._loop = loop or asyncio.get_event_loop()
+        self._producer = confluent_kafka.Producer(configs)
+        self._cancelled = False
+        self._poll_thread = Thread(target=self._poll_loop)
+        self._poll_thread.start()
+
+    def _poll_loop(self):
+        while not self._cancelled:
+            self._producer.poll(0.1)
+
+    def close(self):
+        self._cancelled = True
+        self._poll_thread.join()
+
+    def produce(self, topic, key, value):
+        """
+        An awaitable produce method.
+        """
+        result = self._loop.create_future()
+
+        def ack(err, msg):
+            if err:
+                self._loop.call_soon_threadsafe(result.set_exception, KafkaException(err))
+            else:
+                self._loop.call_soon_threadsafe(result.set_result, msg)
+
+        self._producer.produce(topic=topic, key=key, value=value, on_delivery=ack)
+
+        return result
 
 class KafkaCallback(BackendQueue):
+    SCHEMA_POSTED = []
     def __init__(self, key=None, numeric_type=float, none_to=None, **kwargs):
         """
         You can pass configuration options to AIOKafkaProducer as keyword arguments.
